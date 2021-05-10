cfa1e90f952770672b6e124a88cc8649d33329af
Margoburrito <rchistyakova@gmail.com>
2021-05-10 11:24:42 +0300

    добавление к коммиту "сделана валидация фикстуры перед каждым тестом и доработан вспомогательный класс для операций login и logout"

--- test/test_contact_edit.py
+++ test/test_contact_edit.py
@@ -2,7 +2,5 @@ from model.contact import Contact
 
 
 def test_edit_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_second_contact(Contact(firstname=u"Михаил", middlename=u"Юрьевич", lastname=u"Лермонтов", company="", address=u"г. Пятигорск",
                                             work_tel="897654", e_mail="lermontov.mu@gmail.com", bday="15", bmonth="October", byear="1814", address2="Машук", notes="дуэль"))
-    app.session.logout()
